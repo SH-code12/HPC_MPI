80ba52b38bb59d37094bba2b615ecd8ff162ac52
Malak Hisham <122965150+MalakHisham121@users.noreply.github.com>
2025-04-20 14:21:05 +0200

    Add files via upload

---
+++ Caesar Cipher Console.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <string.h>
+#include <mpi.h>
+
+
+#define TAG 0
+
+#define TAG_OPERATION 1
+#define TAG_LENGTH 2
+
+void encrypt(char *s, int l) {
+    for (int i = 0; i < l; i++) {
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            s[i] = 'a' + (s[i] - 'a' + 3) % 26;
+        } else if (s[i] >= 'A' && s[i] <= 'Z') {
+            s[i] = 'A' + (s[i] - 'A' + 3) % 26;
+        }
+    }
+}
+
+void decrypt(char *s, int l) {
+    for (int i = 0; i < l; i++) {
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            s[i] = 'a' + (s[i] - 'a' - 3+ 26) % 26;
+        } else if (s[i] >= 'A' && s[i] <= 'Z') {
+            s[i] = 'A' + (s[i] - 'A' - 3+ 26) % 26;
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int rank, numofP;
+    char input[500];
+    char result_text[500];
+    int inputL = 0;
+    int op;
+    int  subSize ;
+    char local_text[1000];
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numofP);
+
+    
+    if (rank == 0) {
+        printf("Enter input to process :");
+        scanf("%99s", input);
+        inputL = strlen(input);
+        if (inputL % numofP != 0) {
+            int newL = ((inputL/ numofP ) + 1) * numofP ;
+            for (int i = inputL; i < newL; i++) {
+                input[i] = ' ';
+            }
+            input[newL] = '\0';
+            inputL = newL;
+        }
+        
+        printf("Choose encrypt or decrypt:\n1. Encrypt\n2. Decrypt\nI choose : ");
+        scanf("%d", &op);
+        
+         for (int i = 1; i < numofP; i++) {
+            MPI_Send(&op, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+            MPI_Send(&inputL, 1, MPI_INT, i, 2, MPI_COMM_WORLD);
+        }
+    }
+    else{
+             MPI_Recv(&op, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&inputL, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+
+
+    subSize = inputL / numofP ;
+
+    
+    MPI_Scatter(input, subSize, MPI_CHAR, 
+               local_text,  subSize, MPI_CHAR, 
+               0, MPI_COMM_WORLD);
+
+    local_text[ subSize] = '\0';
+
+    
+    if (op == 1) {
+        encrypt(local_text,  subSize);
+    } else {
+        decrypt(local_text, subSize);
+    }
+
+    
+    MPI_Gather(local_text,  subSize, MPI_CHAR, 
+              result_text,  subSize, MPI_CHAR, 
+              0, MPI_COMM_WORLD);
+
+    
+    if (rank == 0) {
+        result_text[inputL] = '\0';
+        printf("Output: %s\n", result_text);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
